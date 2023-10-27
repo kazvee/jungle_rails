e46485ad0729399b8fadd8e98dc81d8b14ed7282
kazvee <kayvee123@gmail.com>
2023-10-26 18:47:43 -0600

    feat(routes): add user signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,11 @@ Rails.application.routes.draw do
 
   get '/about' => 'about#index'
 
+  # These routes will be for signup. The first renders a form in the browser, the second will 
+  # receive the form and create a user in our database using the data given to us by the user.
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
