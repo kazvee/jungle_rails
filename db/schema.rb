c0a9b9b1b4fbdbe8942d1a7444e1138d121fa910
kazvee <kayvee123@gmail.com>
2023-10-27 11:24:07 -0600

    fix(migration): rollback to rename password column to password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_27_002213) do
+ActiveRecord::Schema.define(version: 2023_10_27_171736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2023_10_27_002213) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
