e994d58044737d7b6f4d039d81f278b5117c43fc
kazvee <kayvee123@gmail.com>
2023-11-06 11:38:27 -0700

    feat(test): product with all required fields validates successfully

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+    it 'is valid with all required fields' do
+      category = Category.new(name: 'Example Category')
+      product = Product.new(
+        name: 'Example Product',
+        price: 100.00,
+        quantity: 10,
+        category: category
+      )
+      expect(product).to be_valid
+    end
   end
 end
