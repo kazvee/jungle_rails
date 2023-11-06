446948ae7fdb0030ab65e604148ec7cfeaeede82
kazvee <kayvee123@gmail.com>
2023-11-06 13:51:00 -0700

    feat(test): refactor tests with Product in before block for DRY code

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,63 +2,44 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    it 'is valid with all required fields' do
-      category = Category.new(name: 'Example Category')
-      product = Product.new(
+
+    # Before block to use the same Product object example for each test
+    before do
+      @category = Category.new(name: 'Example Category')
+      @product = Product.new(
         name: 'Example Product',
         price: 100.00,
         quantity: 10,
-        category: category
+        category: @category
       )
-      expect(product).to be_valid
+    end
+
+    it 'is valid with all required fields' do
+      expect(@product).to be_valid
     end
 
     it 'is not valid when name is nil' do
-      category = Category.new(name: 'Example Category')
-      product = Product.new(
-        name: nil,
-        price: 100.00,
-        quantity: 10,
-        category: category
-      )
-      expect(product).not_to be_valid
-      expect(product.errors.full_messages).to eq(["Name can't be blank"])
+      @product.name = nil
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to eq(["Name can't be blank"])
     end
 
     it 'is not valid when price is nil' do
-      category = Category.new(name: 'Example Category')
-      product = Product.new(
-        name: 'Example Product',
-        price_cents: nil,  # Setting price_cents to nil simulates having price be nil
-        quantity: 10,
-        category: category
-      )
-      expect(product).not_to be_valid
-      expect(product.errors.full_messages).to eq(["Price cents is not a number", "Price is not a number", "Price can't be blank"])
+      @product.price_cents = nil # Setting price_cents to nil simulates having price be nil
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to eq(["Price cents is not a number", "Price is not a number", "Price can't be blank"])
     end
 
     it 'is not valid when quantity is nil' do
-      category = Category.new(name: 'Example Category')
-      product = Product.new(
-        name: 'Example Product',
-        price: 100.00,
-        quantity: nil,
-        category: category
-      )
-      expect(product).not_to be_valid
-      expect(product.errors.full_messages).to eq(["Quantity can't be blank"])
+      @product.quantity = nil
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to eq(["Quantity can't be blank"])
     end
 
     it 'is not valid when category is nil' do
-      category = Category.new(name: 'Example Category')
-      product = Product.new(
-        name: 'Example Product',
-        price: 100.00,
-        quantity: 10,
-        category: nil
-      )
-      expect(product).not_to be_valid
-      expect(product.errors.full_messages).to eq(["Category must exist", "Category can't be blank"])
+      @product.category = nil
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to eq(["Category must exist", "Category can't be blank"])
     end
   end
 end
