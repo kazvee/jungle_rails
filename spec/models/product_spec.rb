ce2b44f0d5e3023d131ce8696ae837edaa84928f
kazvee <kayvee123@gmail.com>
2023-11-06 13:36:59 -0700

    feat(test): product missing a price does not validate successfully

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -24,5 +24,17 @@ RSpec.describe Product, type: :model do
       expect(product).not_to be_valid
       expect(product.errors.full_messages).to eq(["Name can't be blank"])
     end
+
+    it 'is not valid when price is nil' do
+      category = Category.new(name: 'Example Category')
+      product = Product.new(
+        name: 'Example Product',
+        price_cents: nil,  # Setting price_cents to nil simulates having price be nil
+        quantity: 10,
+        category: category
+      )
+      expect(product).not_to be_valid
+      expect(product.errors.full_messages).to eq(["Price cents is not a number", "Price is not a number", "Price can't be blank"])
+    end
   end
 end
