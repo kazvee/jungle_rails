db32cb25587e7df112cc5800cdf3e86aa4d5ac7c
kazvee <kayvee123@gmail.com>
2023-11-06 15:36:26 -0700

    feat(test): add test for case where both password fields do not match

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,5 +9,14 @@ RSpec.describe User, type: :model do
       )
       expect(user).to be_valid
     end
+
+    it 'is not valid if password and password_confirmation fields do not match' do
+      user = User.new(
+        password: 'password123',
+        password_confirmation: 'password456'
+      )
+      expect(user).not_to be_valid
+      expect(user.errors.full_messages).to eq(["Password confirmation doesn't match Password"])
+    end
   end
 end
