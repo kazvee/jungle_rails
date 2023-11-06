1f75d112617c1d59b7ff11136d7081988cf2cc8c
kazvee <kayvee123@gmail.com>
2023-11-06 11:45:56 -0700

    feat(test): product missing a name does not validate successfully

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,5 +12,17 @@ RSpec.describe Product, type: :model do
       )
       expect(product).to be_valid
     end
+
+    it 'is not valid when name is nil' do
+      category = Category.new(name: 'Example Category')
+      product = Product.new(
+        name: nil,
+        price: 100.00,
+        quantity: 10,
+        category: category
+      )
+      expect(product).not_to be_valid
+      expect(product.errors.full_messages).to eq(["Name can't be blank"])
+    end
   end
 end
