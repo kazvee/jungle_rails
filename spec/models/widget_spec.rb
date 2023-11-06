fd69a0cb8be2e855e913c4e73e4deba982859c52
kazvee <kayvee123@gmail.com>
2023-11-05 18:43:43 -0700

    test(models): add example spec for Widget model

--- spec/models/widget_spec.rb
+++ spec/models/widget_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Widget, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe '#id' do
+  it 'does not exist for new records' do
+    @widget = Widget.new
+    expect(@widget.id).to be_nil
+  end
+
+  it 'is auto-assigned by AR for saved records' do
+    @widget = Widget.new
+    # We use bang here because we want our spec to fail if save fails (due to validations)
+    # We are not testing for successful save so we have to assume it will be successful
+    @widget.save!
+
+    expect(@widget.id).to be_present
+  end
 end
