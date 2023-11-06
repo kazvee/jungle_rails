ca3b937a22c942b2ec8f4c0be3507dc677623078
kazvee <kayvee123@gmail.com>
2023-11-06 15:01:48 -0700

    feat(test): user with required password fields validates successfully

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'is valid with password and password_confirmation fields' do
+      user = User.new(
+        password: 'password123',
+        password_confirmation: 'password123'
+      )
+      expect(user).to be_valid
+    end
+  end
 end
