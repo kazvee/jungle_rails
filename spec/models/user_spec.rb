ccc956bd024c47f8f8ee9089942b170dbee2eb9b
kazvee <kayvee123@gmail.com>
2023-11-07 09:41:46 -0700

    feat(test): add test for case where user password field is missing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,5 +18,14 @@ RSpec.describe User, type: :model do
       expect(user).not_to be_valid
       expect(user.errors.full_messages).to eq(["Password confirmation doesn't match Password"])
     end
+
+    it 'is not valid if password field is nil' do
+      user = User.new(
+        password: nil,
+        password_confirmation: 'password456'
+      )
+      expect(user).not_to be_valid
+      expect(user.errors.full_messages).to eq(["Password can't be blank"])
+    end
   end
 end
