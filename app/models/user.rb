dc105ee248fcdd513adfb7ebbcec4d83c90a40f3
kazvee <kayvee123@gmail.com>
2023-11-09 15:51:08 -0700

    feat(model): update User model with validation requirements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :password, length: { minimum: 6 }  
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password_confirmation, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
